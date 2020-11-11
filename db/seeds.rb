0d1235323854c1a1d8f7d778ce02fcae37de7b04
oliver coton <oliver.coton@gmail.com>
2020-11-11 16:51:04 +0100

    fully functioning but bloody ugly airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'executing seed'
+puts 'cleaning database'
+Flat.destroy_all
+puts 'database is clean'
+
+puts 'creating flats'
+5.times do
+  flat = Flat.create(
+    name: Faker::Quote.robin,
+    address: Faker::Address.city,
+    description: Faker::Quote.famous_last_words,
+    price_per_night: rand(50..250),
+    number_of_guests: rand(1..6),
+  )
+  puts "flat #{flat.id} generated"
+end
